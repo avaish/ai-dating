47b1f978d7579ef7ea29f1397e6066bbb53ed9ba
Atharv Vaish <atharv.vaish@gmail.com>
2023-12-28 03:41:41 -0500

    fix openai api key env var

--- src/env.js
+++ src/env.js
@@ -36,6 +36,7 @@ export const env = createEnv({
   runtimeEnv: {
     DATABASE_URL: process.env.DATABASE_URL,
     NODE_ENV: process.env.NODE_ENV,
+    OPENAI_API_KEY: process.env.OPENAI_API_KEY,
     // NEXT_PUBLIC_CLIENTVAR: process.env.NEXT_PUBLIC_CLIENTVAR,
   },
   /**

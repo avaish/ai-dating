285f613f5a2269727985289121b3677911470cae
Stephen Worlow <stephen.worlow@benchling.com>
2024-03-03 09:46:11 -0500

    More models

--- sandbox/ai-dating/src/models/base_model.py
+++ sandbox/ai-dating/src/models/base_model.py
@@ -7,5 +7,37 @@ import sqlalchemy.orm as so
 
 class BaseModel(db.Model):
     __abstract__ = True
-    created_at: so.Mapped[datetime] = so.mapped_column(default=lambda: datetime.now(timezone.utc))
-    updated_at: so.Mapped[datetime] = so.mapped_column(default=lambda: datetime.now(timezone.utc))
+    id = db.Column(db.Integer,
+                   primary_key=True,
+                   autoincrement=True)
+    created_at = db.Column(db.DateTime(timezone=True), nullable=False)
+    updated_at = db.Column(db.DateTime(timezone=True), nullable=False)
+
+    def before_save(self, *args, **kwargs):
+        current_time = datetime.now(timezone.utc)
+        if not self.created_at:
+            self.created_at = current_time
+        self.updated_at = current_time
+
+    def after_save(self, *args, **kwargs):
+        pass
+
+    def save(self, commit=True):
+        self.before_save()
+        db.session.add(self)
+        if commit:
+            try:
+                db.session.commit()
+            except Exception as e:
+                db.session.rollback()
+                raise e
+
+        self.after_save()
+
+    def delete(self, commit=True):
+        db.session.delete(self)
+        if commit:
+            db.session.commit()
+    
+
+    
--- sandbox/ai-dating/src/models/users.py
+++ sandbox/ai-dating/src/models/users.py
@@ -8,7 +8,6 @@ from src.models.base_model import BaseModel
 
 
 class User(BaseModel):
-    id: so.Mapped[int] = so.mapped_column(primary_key=True)
     username: so.Mapped[str] = so.mapped_column(sa.String(64), index=True,
                                                 unique=True)
     email: so.Mapped[str] = so.mapped_column(sa.String(120), index=True,

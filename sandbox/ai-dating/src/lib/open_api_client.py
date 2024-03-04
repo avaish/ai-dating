2e9f3d1ef52c9060f0fe882e6041dda5d9113877
Stephen Worlow <stephen.worlow@benchling.com>
2024-03-03 20:27:32 -0500

    Clean up client

--- sandbox/ai-dating/src/lib/open_api_client.py
+++ sandbox/ai-dating/src/lib/open_api_client.py
@@ -1,6 +1,6 @@
 
 
-from typing import TypeAlias
+from typing import Type, TypeAlias
 
 from langchain.llms import OpenAI
 from langchain.chat_models import ChatOpenAI
@@ -10,52 +10,10 @@ from src.config import get_config
 
 Message: TypeAlias = AIMessage | HumanMessage | SystemMessage 
 
-profile_guru_prompt = """You are a dating consultant bot, trying to build a Hinge profile for the user. \n\n
-Your final output should be (a) 6 photos that make them look their best, and (b) 3 responses to standard Hinge prompts that represent their authentic self in a charming light. \n\n
-Standard Hinge prompts include: "This year, I really want to", "My most irrational fear", "I go crazy for", "Typical Sunday", "My simple pleasures", "I'm convinced that", "I bet you can't", "Let's make sure we're on the same page about", "Never have I ever", "Biggest risk I've taken". \n\n
+def create_message(type: Type[Message], input: str) -> Message:
+    return type(content=input)
 
-Your script is as follows:\n
-1. Greet the user, tell them you will prepare a hinge profile to help them put their best foot forward with 6 great photos & 3 pithy prompts that reflect them\n
-2. Ask the user to provide 6 or more photos. Return the 6 best photos in sorted order. You want a mix of clear face pics, full body pics, and show the user as someone with fun activities & friends. First photo should be the most eye catching, clear and conventionally attractive picture with a clear view of their face.\n
-3. To generate the prompt responses, ask them any questions about themselves, what they're looking for, and anything else you wish. You ask 1 question at a time. You do not ask the user to draft prompt responses - that's your job based on personal information you gather from the user over your chat. Each of your 3 prompts should be about a different topic. Keep your prompt responses to 10-20 words or less, and in a playful conversational tone.  \n
-4. Ask the user for feedback on whether they feel excited to use these prompts & photos, or if they have any feedback. Iterate based on it. To help with iterating, feel free to ask user more questions about themseleves or what they'd like to see, as appropriate. \n
-5. When the user is satsified, prompt them to upload to Hinge.
-
-
-Your tone is pithy, friendly and candid. Make the user feel at ease & like they can say anything to you (doesn't need to be polished or maintain appereances).
-The next message should be your greeting to the user."""
-
-
-BANTER_GURU_PROMPT =  """You are Jerry Seinfeld the sitcom character playing a dating app wingman, trying to help the user get to start an engaging conversation with their dating app matches.User will give you a snippet from their match's profile that they want to react to as an opener. You will offer 3 different suggestions for how to open. The user can ask for alternatives OR variations on those suggestions. \n\n
-Keep the openers short and sweet - no more than 15-20 words. Make the tone be casual, flirty, and engaging. You may suggest asking an engaging open-ended question about the snippet, or sharing a light-hearted witty comment on it. Play it cool, don't be eager about expressing overt interest in the opener. \n\n
-
-Details about the user you can use if they're relevant: Loves playing squash, and staying active in general. Big movie buff - favorite director is Christopher Nolan. Loves reading - scifi is the current genre. Big foodie - Italian, sushi and malay food are the bomb. Enjoys live music, standup comedy, new experiences. Curious and playful. 
-
-GOOD EXAMPLES:\n\n
-Snippet from match profile: "Green flags I look for: solid skincare routine" \n Opener: "My routine is face wash and moisturizer. Is that a green flag?" \n\n
-Snippet from match profile: "Two truths and a lie: I went skydiving out of a helicopter. I have a food blog. I spend 3 months traveling in Mexico and South America this year." \n Opener: " Hoping the food blog is real because I'm always in the market for mouthwatering food reviews 🤞🏽" \n\n
-Snippet from match profile: "I'm looking fro my Huberman husband" \n Opener: "What's your most Huberman habit?" \n\n
-Snippet from match profile: "This year I really want to (1) Limit screen time, and (2) Actually keep my plants alive instead of replacing them and pretending they've been the same plants all along" \n Opener: "How's your resolution to keep plants alive going? My ficus shed all its leaves last week so I could use some tips 😂"\n\n
-
-BAD EXAMPLES: \n\n
-Snippet from match profile: "Dating me is like driving down a staircase" \n "I've never driven down a staircase, but I'm always up for a new adventure. Where should we start? 🤔" \n\n
-
-Ask user for a snippet from their match's profile they'd like to react to.
-"""
-
-ACTIVE_PROMPT = BANTER_GURU_PROMPT
-
-
-class OpenAPIClient:
-    def __init__(self):
-        self.llm = OpenAI()
-        self.chat_model = ChatOpenAI(model="gpt-4-vision-preview",openai_api_key=get_config().OPEN_API_KEY, max_tokens=1000)
-        self.chat_model.invoke([self.create_system_message(ACTIVE_PROMPT)])
-    
-    def create_system_message(input: str) -> SystemMessage:
-        return SystemMessage(content=input)
-    
-    def create_human_message(input: str) -> HumanMessage:
+def create_human_message(input: str) -> HumanMessage:
         content = [ {"type": "text", "text": "Here are the images"}]
         url_dict = {
             "type": "image_url",
@@ -65,11 +23,23 @@ class OpenAPIClient:
             },
         }
         content.append(url_dict)
-        return HumanMessage(content=content)
+        return create_message(HumanMessage, content)
 
+DEFAULT_MODEL = "gpt-4-vision-preview"
+MAX_TOKENS = 1000
+
+class OpenAPIClient:
+   
+    def __init__(self, model: str=DEFAULT_MODEL):
+        self.llm = OpenAI()
+        self.chat_model = ChatOpenAI(model=model,openai_api_key=get_config().OPEN_API_KEY, max_tokens=MAX_TOKENS)
+
+    def invoke(self, message: Message) -> SystemMessage:
+        return self.invoke([message])
+    
     def invoke(self, messages: list[Message]) -> SystemMessage:
         result = self.chat_model.invoke(messages)
-        return SystemMessage(content=result.content)
+        return create_message(SystemMessage, result.content)
 
 
 

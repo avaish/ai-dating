158d0b50210ddab4234b02b83745088f5a80e453
Stephen Worlow <stephen.worlow@benchling.com>
2024-03-09 02:38:54 -0500

    Update chats

--- sandbox/ai-dating/src/routers/chats.py
+++ sandbox/ai-dating/src/routers/chats.py
@@ -15,25 +15,29 @@ router = APIRouter(
     responses={404: {"description": "Not found"}},
 )
 
+@router.get("/{session_id}:image")
+async def do_chat(session_id: str):
+    open_api_client = get_open_api_client()
+    chat = open_api_client.chat_model
+
+    system_message = create_system_message(PROFILE_GURU_PROMPT)
+    human_message = create_image_message()
+
+    PROFILE_GURU_PROMPT_TEMPLATE = ChatPromptTemplate.from_messages([system_message, human_message])
+    chain = PROFILE_GURU_PROMPT_TEMPLATE | chat
+    ai_message = chain.invoke({})
+
+    chat_session = ChatSession(session_id, system_message)
+    chat_session.add_ai_message(ai_message)
+    print(ai_message)
+
+
 @router.get("/{session_id}")
-async def do_chat(session_id: str, text: str, image: bool):
+async def do_chat(session_id: str, text: str):
     system_message = create_system_message(PROFILE_GURU_PROMPT)
     chat_session = ChatSession(session_id, system_message)
-    if image:
-        open_api_client = get_open_api_client()
-        chat = open_api_client.chat_model
+    ai_message = chat_session.invoke(text)
 
-        system_message = create_system_message(PROFILE_GURU_PROMPT)
-        human_message = create_image_message()
+    print(ai_message)
 
-        PROFILE_GURU_PROMPT_TEMPLATE = ChatPromptTemplate.from_messages([system_message, human_message])
-        chain = PROFILE_GURU_PROMPT_TEMPLATE | chat
-        ai_message = chain.invoke({})
-        chat_session.add_ai_message(ai_message)
-
-        print(chat_session.chat_history)
-    else:
-        ai_message = chat_session.invoke(text)
-        print(ai_message)
-
-        print(chat_session.chat_history)
+    print(chat_session.chat_history)

bdee5a606fa51f963b4820cc47924690adf05d12
Stephen Worlow <stephen.worlow@benchling.com>
2024-03-09 03:20:29 -0500

    Clean up posts API

--- sandbox/ai-dating/src/routers/posts.py
+++ sandbox/ai-dating/src/routers/posts.py
@@ -1,15 +1,8 @@
 from fastapi import APIRouter, Depends, HTTPException
 
-from langchain_core.prompts import ChatPromptTemplate
-
 
 from src.models.base_model import Repository
 from src.models.posts import Post, create_post_repository
-from src.lib.chat_session import ChatSession
-from src.lib.image_utils import create_image_message
-from src.lib.message_utils import create_human_message, create_system_message
-from src.lib.open_api_client import get_open_api_client
-from src.lib.prompts import PROFILE_GURU_PROMPT
 
 
 router = APIRouter(
@@ -20,38 +13,9 @@ router = APIRouter(
 
 fake_posts_db = {"plumbus": {"name": "Plumbus"}, "gun": {"name": "Portal Gun"}}
 
-@router.get("/{session_id}")
-async def read_posts(session_id: str, post_repository: Repository[Post] = Depends(create_post_repository)):
-    open_api_client = get_open_api_client()
-    chat = open_api_client.chat_model
-
-    system_message = create_system_message(PROFILE_GURU_PROMPT)
-    human_message = create_image_message()
-
-    PROFILE_GURU_PROMPT_TEMPLATE = ChatPromptTemplate.from_messages([system_message, human_message])
-    chain = PROFILE_GURU_PROMPT_TEMPLATE | chat
-    ai_message = chain.invoke({})
-    print(ai_message)
-
-    chat_session = ChatSession(session_id, system_message)
-    chat_session.add_ai_message(ai_message)
-    ai_message = chat_session.invoke("I love programming.")
-    print(ai_message)
-
-    ai_message = chat_session.invoke("I volunteer for an animal shelter")
-    print(ai_message)
-
-    ai_message = chat_session.invoke("I own a yacht")
-    print(ai_message)
-
-    ai_message = chat_session.invoke("Can you summarize the photos and the prompts we just made? Thanks!")
-    print(ai_message)
-
-    print(chat_session.chat_history)
-
+@router.get("/")
+async def read_posts(post_repository: Repository[Post] = Depends(create_post_repository)):
     return post_repository.list()
-
-    # return fake_posts_db
 
 
 @router.get("/{post_id}")

8a31d4381eebd31bccb25cb5ca265893295d3964
Stephen Worlow <stephen.worlow@benchling.com>
2024-03-09 03:17:28 -0500

    Abstract out

--- sandbox/ai-dating/src/routers/chats.py
+++ sandbox/ai-dating/src/routers/chats.py
@@ -1,13 +1,23 @@
+from enum import Enum
+
 from fastapi import APIRouter
-
 from langchain_core.prompts import ChatPromptTemplate
-
 
 from src.lib.chat_session import ChatSession
 from src.lib.image_utils import create_image_message
 from src.lib.message_utils import create_system_message
 from src.lib.open_api_client import get_open_api_client
-from src.lib.prompts import PROFILE_GURU_PROMPT
+from src.lib.prompts import BANTER_GURU_PROMPT, PROFILE_GURU_PROMPT
+
+class ChatMode(Enum):
+    BANTER = 'banter'
+    PROFILE_CREATION = 'profile-creation'
+
+CHAT_MODE_TO_PROMPT = {
+    ChatMode.BANTER: BANTER_GURU_PROMPT,
+    ChatMode.PROFILE_CREATION: PROFILE_GURU_PROMPT
+}
+
 
 router = APIRouter(
     prefix="/chats",
@@ -16,15 +26,15 @@ router = APIRouter(
 )
 
 @router.get("/{session_id}:image")
-async def do_chat(session_id: str):
+async def do_chat(session_id: str, chat_mode: ChatMode):
     open_api_client = get_open_api_client()
     chat = open_api_client.chat_model
 
-    system_message = create_system_message(PROFILE_GURU_PROMPT)
+    system_message = create_system_message(CHAT_MODE_TO_PROMPT[chat_mode])
     human_message = create_image_message()
 
-    PROFILE_GURU_PROMPT_TEMPLATE = ChatPromptTemplate.from_messages([system_message, human_message])
-    chain = PROFILE_GURU_PROMPT_TEMPLATE | chat
+    prompt = ChatPromptTemplate.from_messages([system_message, human_message])
+    chain = prompt | chat
     ai_message = chain.invoke({})
 
     chat_session = ChatSession(session_id, system_message)
@@ -33,8 +43,8 @@ async def do_chat(session_id: str):
 
 
 @router.get("/{session_id}")
-async def do_chat(session_id: str, text: str):
-    system_message = create_system_message(PROFILE_GURU_PROMPT)
+async def do_chat(session_id: str, chat_mode: ChatMode, text: str):
+    system_message = create_system_message(CHAT_MODE_TO_PROMPT[chat_mode])
     chat_session = ChatSession(session_id, system_message)
     ai_message = chat_session.invoke(text)
 

bcec1fd7d7b778d8e9130fdacc0122238e5ac4ff
Stephen Worlow <stephen.worlow@benchling.com>
2024-03-09 04:05:23 -0500

    More config

--- sandbox/ai-dating/src/config.py
+++ sandbox/ai-dating/src/config.py
@@ -5,11 +5,11 @@ class Config:
     DEBUG = True
     TESTING = False
 
-    DB_USER=os.environ("DB_USER")
-    DB_PASSWORD=os.environ("DB_PASSWORD")
-    DB_HOST=os.environ("DB_HOST")
-    DB_PORT=os.environ("DB_PORT")
-    DB_NAME=os.environ("DB_NAME")
+    DB_USER=os.environ.get("DB_USER")
+    DB_PASSWORD=os.environ.get("DB_PASSWORD")
+    DB_HOST=os.environ.get("DB_HOST")
+    DB_PORT=os.environ.get("DB_PORT")
+    DB_NAME=os.environ.get("DB_NAME")
     OPEN_API_KEY = os.environ.get("OPENAI_API_KEY")
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
 
